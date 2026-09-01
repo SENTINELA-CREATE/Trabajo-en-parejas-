be041122657042bc3e7ed2cd76ad373555847724
SENTINELA-CREATE <juansebastianmarulandamolina09@gmail.com>
2026-09-01 12:59:02 -0500

    Creacion del proyecto para el trabajo evaluativo

---
+++ trabajo-evaluativo/src/main/java/com/example/trabajo_evaluativo/TrabajoEvaluativoApplication.java
@@ -0,0 +1,13 @@
+package com.example.trabajo_evaluativo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabajoEvaluativoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabajoEvaluativoApplication.class, args);
+	}
+
+}
---
+++ trabajo-evaluativo/src/test/java/com/example/trabajo_evaluativo/TrabajoEvaluativoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.trabajo_evaluativo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabajoEvaluativoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
