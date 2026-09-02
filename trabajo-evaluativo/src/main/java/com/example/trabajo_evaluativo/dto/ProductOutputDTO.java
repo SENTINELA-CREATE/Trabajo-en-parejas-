36c0713fb9808057319d463e1e6335d361d069f7
albeiro02 <bluesky02user@gmail.com>
2026-09-02 10:57:36 -0500

    se agregó ProductOutputDTO donde se ubican los atributos correspondientes a la salida.. especificamento los atributos de id y creationDate

---
+++ trabajo-evaluativo/src/main/java/com/example/trabajo_evaluativo/dto/ProductOutputDTO.java
@@ -0,0 +1,18 @@
+package com.example.trabajo_evaluativo.dto;
+
+import lombok.*;
+import java.time.LocalDateTime;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+
+public class ProductOutputDTO {
+    private Long id;
+    private String name;
+    private String description;
+    private Double price;
+    private Integer quantity;
+    private String category;
+    private LocalDateTime creationDate;
+}
