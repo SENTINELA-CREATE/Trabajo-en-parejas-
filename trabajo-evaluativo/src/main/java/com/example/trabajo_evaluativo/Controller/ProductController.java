c4232f31ea3faabd78ca811c5f5edcaf6c599a7f
albeiro02 <bluesky02user@gmail.com>
2026-09-03 18:55:25 -0500

    correcciones y mejoras esteticas en el proyecto

--- trabajo-evaluativo/src/main/java/com/example/trabajo_evaluativo/Controller/ProductController.java
+++ trabajo-evaluativo/src/main/java/com/example/trabajo_evaluativo/Controller/ProductController.java
@@ -10,9 +10,10 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/products")
 @RequiredArgsConstructor
-@RequestMapping("/api/products")
 public class ProductController {
+
     private final ProductService productService;
 
     @PostMapping
@@ -33,6 +34,18 @@ public class ProductController {
         return ResponseEntity.ok(product);
     }
 
+    @GetMapping("/category/{category}")
+    public ResponseEntity<List<ProductOutputDTO>> getProductsByCategory(@PathVariable String category) {
+        List<ProductOutputDTO> products = productService.findByCategory(category);
+        return ResponseEntity.ok(products);
+    }
+
+    @GetMapping("/search")
+    public ResponseEntity<List<ProductOutputDTO>> searchProductsByName(@RequestParam String name) {
+        List<ProductOutputDTO> products = productService.findByName(name);
+        return ResponseEntity.ok(products);
+    }
+
     @PutMapping("/{id}")
     public ResponseEntity<ProductOutputDTO> updateProduct(@PathVariable Long id, @RequestBody ProductInputDTO dto) {
         ProductOutputDTO updatedProduct = productService.updateProduct(id, dto);
@@ -42,15 +55,6 @@ public class ProductController {
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteProduct(@PathVariable Long id) {
         productService.deleteById(id);
-        return ResponseEntity.ok("Product eliminado correctamente");
-    }
-    @GetMapping("/category/{category}")
-    public ResponseEntity<List<ProductOutputDTO>> getProductsByCategory(@PathVariable String category) {
-        return ResponseEntity.ok(productService.findByCategory(category));
-    }
-
-    @GetMapping("/search")
-    public ResponseEntity<List<ProductOutputDTO>> searchProductsByName(@RequestParam String name) {
-        return ResponseEntity.ok(productService.findByName(name));
+        return ResponseEntity.ok("Producto eliminado correctamente");
     }
 }
